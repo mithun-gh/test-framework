ac282aed8ef0f2b8150ec38580ddd15689277e57
Mithun <mithunrathinasamy@live.com>
2020-04-26 15:05:52 +1000

    refactor Template class

--- framework/template.ts
+++ framework/template.ts
@@ -1,64 +1,60 @@
-export function render(template: TemplateResult, selector: string) {
+export function render(template: Template, selector: string) {
   const element = document.querySelector(selector);
   if (element === null) {
     throw new Error(`Invalid selector: ${selector}`);
   }
-  element.insertAdjacentHTML("afterbegin", template.html);
+  element.insertAdjacentHTML("afterbegin", template.getHtml());
 }
 
-class TemplateResult {
-  html: string;
-  constructor(html: string) {
-    this.html = html;
+class Template {
+  private readonly strings: TemplateStringsArray;
+  private readonly values: readonly unknown[];
+
+  constructor(strings: TemplateStringsArray, values: readonly unknown[]) {
+    this.strings = strings;
+    this.values = values;
+  }
+
+  getHtml(): string {
+    let html = "";
+
+    this.strings.forEach((s: unknown, i: number) => {
+      const v = this.values[i];
+      html += s + this.transform(v);
+    });
+
+    return html;
+  }
+
+  private escape(html: string): string {
+    return html
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;");
+  }
+
+  private transform(value: unknown): string {
+    if (Array.isArray(value)) {
+      return value.map((v) => this.transform(v)).join("");
+    }
+
+    if (value instanceof Template) {
+      return value.getHtml();
+    }
+
+    if (typeof value === "function") {
+      const fn = value.toString().replace(/"/g, "'");
+      return `"(${fn})(event)"`;
+    }
+
+    if (value != null) {
+      return this.escape(value.toString());
+    }
+
+    return "";
   }
 }
 
-export function css(
-  strings: TemplateStringsArray,
-  ...values: unknown[]
-): string {
-  return `<style>${strings.join()}</style>`;
-}
-
-export function html(
-  strings: TemplateStringsArray,
-  ...values: unknown[]
-): TemplateResult {
-  let result = "";
-
-  strings.forEach((s: unknown, i: number) => {
-    const v = values[i];
-    result += s + transform(v);
-  });
-
-  return new TemplateResult(result);
-}
-
-function transform(value: unknown): string {
-  if (Array.isArray(value)) {
-    return value.map((v) => transform(v)).join("");
-  }
-
-  if (value instanceof TemplateResult) {
-    return value.html;
-  }
-
-  if (typeof value === "function") {
-    const fn = value.toString().replace(/"/g, "'");
-    return `"(${fn})(event)"`;
-  }
-
-  if (value != null) {
-    return escape(value.toString());
-  }
-
-  return "";
-}
-
-function escape(html: string): string {
-  return html
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;");
-}
+export const html = (strings: TemplateStringsArray, ...values: unknown[]) =>
+  new Template(strings, values);
