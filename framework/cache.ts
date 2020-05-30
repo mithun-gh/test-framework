9fad75dd84a24ec884dc138efc5655eea9495189
Mithun <mithunrathinasamy@live.com>
2020-05-30 19:53:41 +1000

    add Fragment cache

--- framework/cache.ts
+++ framework/cache.ts
@@ -1,3 +1,5 @@
 import { Template } from "./template";
+import { Fragment } from "./fragment";
 
 export const TemplateCache: WeakMap<TemplateStringsArray, Template> = new WeakMap();
+export const FragmentCache: WeakMap<Template, Fragment> = new WeakMap();
