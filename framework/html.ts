54a7e98e7ae5dd089c7579f7561306082b3f2d0b
Mithun <mithunrathinasamy@live.com>
2020-05-31 14:56:51 +1000

    move code from html to Template

--- framework/html.ts
+++ framework/html.ts
@@ -1,76 +1,13 @@
 import { TemplateCache } from "./cache";
-import { Metadata, MetadataType } from "./metadata";
-import { isOpenTagEnd } from "./utils";
-import { event, attribute } from "./regex";
 import { Template } from "./template";
-import { Sentinel } from "./sentinel";
 
-export function html(strings: TemplateStringsArray, ...values: readonly unknown[]): Template {
+export function html(strings: TemplateStringsArray, ...values: unknown[]): Template {
   if (TemplateCache.has(strings)) {
-    const template = TemplateCache.get(strings);
-    const newValues = transformValues(values, template.metadata);
-    return TemplateCache.get(strings).updateValues(newValues);
+    return TemplateCache.get(strings).updateValues(values);
   }
 
-  let metadata: Metadata[] = values.map((value, i) => {
-    let key: string;
-    let str: string = strings.raw[i];
-    const nextStr: string = strings.raw[i + 1];
-    const isLastAttr = isOpenTagEnd(nextStr) ?? true;
-    if ((key = str.match(event)?.[1])) {
-      return new Metadata(MetadataType.Event, [key, isLastAttr]);
-    } else if ((key = str.match(attribute)?.[1])) {
-      return new Metadata(MetadataType.Attribute, [key, isLastAttr]);
-    } else if (value instanceof Template) {
-      return new Metadata(MetadataType.Template);
-    } else if (Array.isArray(value)) {
-      return new Metadata(MetadataType.Template, [i]);
-    } else {
-      return new Metadata(MetadataType.Text);
-    }
-  });
-
-  let slotIndex: number = -1;
-  const sentinel = new Sentinel();
-  let string = strings.raw.join(sentinel.simple).replace(sentinel.regex, () => {
-    const meta = metadata[++slotIndex];
-    if (meta.type === MetadataType.Attribute || meta.type === MetadataType.Event) {
-      return meta.value[1] ? `${sentinel.attribute}="${slotIndex}"` : "";
-    } else {
-      return `<template ${sentinel.attribute}="${slotIndex}"></template>`;
-    }
-  });
-
-  const newValues = transformValues(values, metadata);
-  const template = new Template(string, newValues, metadata, sentinel);
-
+  const template = new Template(strings, values);
   TemplateCache.set(strings, template);
 
   return template;
-}
-
-function transformValues(values: readonly unknown[], metadata: readonly Metadata[]): unknown[] {
-  const newValues = [...values];
-  metadata
-    .filter((meta) => meta.type === MetadataType.Template && meta.value.length > 0)
-    .forEach((meta) => {
-      let subString = "";
-      const subMeta = [];
-      const subValues = [];
-      const subSentinel = new Sentinel();
-      const templateIndex = meta.value[0];
-      const templateArray = newValues[templateIndex] as unknown[];
-
-      templateArray.forEach((value) => {
-        if (value instanceof Template) {
-          subString += value.string.replace(value.sentinel.attribute, subSentinel.attribute);
-          subValues.push(...value.values);
-          subMeta.push(...value.metadata);
-        } else {
-          subString += String(value);
-        }
-      });
-      newValues.splice(templateIndex, 1, new Template(subString, subValues, subMeta, subSentinel));
-    });
-  return newValues;
 }
--- framework/template.ts
+++ framework/template.ts
@@ -1,37 +1,76 @@
-import { Metadata } from "./metadata";
+import { Metadata, MetadataType } from "./metadata";
 import { Sentinel } from "./sentinel";
+import { isOpenTagEnd } from "./utils";
+import { attribute, event } from "./regex";
+import { html } from "./html";
 
 export class Template {
-  values: readonly unknown[];
+  strings: TemplateStringsArray;
+  values: unknown[];
 
-  readonly string: string;
   readonly metadata: readonly Metadata[];
   readonly sentinel: Sentinel;
 
-  constructor(
-    string: string,
-    values: readonly unknown[],
-    metadata: readonly Metadata[],
-    sentinel: Sentinel
-  ) {
-    this.string = string;
-    this.values = values;
-    this.metadata = metadata;
-    this.sentinel = sentinel;
+  constructor(strings: TemplateStringsArray, values: unknown[]) {
+    this.strings = strings;
+    this.sentinel = new Sentinel();
+    this.metadata = this.getMetadata(values);
+    this.values = this.transformValues(values);
   }
 
-  duplicate(values: readonly unknown[]): Template {
-    return new Template(this.string, values, this.metadata, this.sentinel);
-  }
-
-  updateValues(newValues: readonly unknown[]): Template {
-    this.values = newValues;
+  updateValues(values: unknown[]): Template {
+    this.values = this.transformValues(values);
     return this;
   }
 
   createElement(): DocumentFragment {
     const template = document.createElement("template");
-    template.innerHTML = this.string;
+    template.innerHTML = this.getHtml();
     return template.content.cloneNode(true) as DocumentFragment;
+  }
+
+  transformValues(values: unknown[]): unknown[] {
+    const newValues = [...values];
+    this.metadata
+      .filter((meta) => meta.type === MetadataType.Template && meta.value.length > 0)
+      .forEach((meta) => {
+        const template = html``;
+        const templateIndex = meta.value[0];
+        newValues.splice(templateIndex, 1, template);
+      });
+    return newValues;
+  }
+
+  getMetadata(values: unknown[]): Metadata[] {
+    return values.map((value, i) => {
+      let key: string;
+      let str: string = this.strings.raw[i];
+      const nextStr: string = this.strings.raw[i + 1];
+      const isLastAttr = isOpenTagEnd(nextStr) ?? true;
+      if ((key = str.match(event)?.[1])) {
+        return new Metadata(MetadataType.Event, [key, isLastAttr]);
+      } else if ((key = str.match(attribute)?.[1])) {
+        return new Metadata(MetadataType.Attribute, [key, isLastAttr]);
+      } else if (value instanceof Template) {
+        return new Metadata(MetadataType.Template);
+      } else if (Array.isArray(value)) {
+        return new Metadata(MetadataType.Template, [i]);
+      } else {
+        return new Metadata(MetadataType.Text);
+      }
+    });
+  }
+
+  getHtml(): string {
+    let slotIndex: number = -1;
+    const sentinel = this.sentinel;
+    return this.strings.raw.join(sentinel.simple).replace(sentinel.regex, () => {
+      const meta = this.metadata[++slotIndex];
+      if (meta.type === MetadataType.Attribute || meta.type === MetadataType.Event) {
+        return meta.value[1] ? `${sentinel.attribute}="${slotIndex}"` : "";
+      } else {
+        return `<template ${sentinel.attribute}="${slotIndex}"></template>`;
+      }
+    });
   }
 }
